668fbec9acde5d651f26748bb774fdaddde4040c
adbernardi <anthony.d.bernardi@gmail.com>
2024-09-19 15:17:37 -0400

    Adding model code

---
+++ Titanic/Model_Script_1.py
@@ -0,0 +1,15 @@
+'''
+
+This will serve as the first Python code file for the Kaggle Competition Titantic Prediction. 
+
+This will include everything from data cleaning, feature engineering, initial analysis, all the way through model building, tuning, and deployment.
+
+We'll start with the data now. 
+
+
+'''
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
